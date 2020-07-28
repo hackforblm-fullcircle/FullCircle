a59d7679492de6f836eaa02377b0ef0dfd9c8df7
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-07-28 13:10:56 -0400

    creates UserAction model

--- FullCircle/FullCircle/Models/UserAction.swift
+++ FullCircle/FullCircle/Models/UserAction.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+struct UserAction {
+    let id: String
+    let actionID: String
+    let userID: String
+    let isCompleted: Bool
+    let completionDate: Date?
+    
+    // Initializers
+    // Init UserAction by saving before completion
+    init(id: String, actionID: String, userID: String, isCompleted: Bool, completionDate: Date?) {
+        self.id = "userAction-\(UUID().description)"
+        self.actionID = actionID
+        self.userID = userID
+        self.isCompleted = false
+        self.completionDate = nil
+    }
+    
+    // TODO: Create custom init for UserActions when saved without completing and when completing before saving
+}
