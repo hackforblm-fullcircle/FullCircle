6b01112bbccad57eea0d64679b0975674b96b08b
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-07-28 13:09:17 -0400

    creates AppUser.swift, Cause.swift, UserAction.swift

---
+++ FullCircle/FullCircle/Models/AppUser.swift
@@ -0,0 +1,9 @@
+//
+//  AppUser.swift
+//  FullCircle
+//
+//  Created by Sunni Tang on 7/28/20.
+//  Copyright © 2020 FullCircle. All rights reserved.
+//
+
+import Foundation
---
+++ FullCircle/FullCircle/Models/UserAction.swift
@@ -0,0 +1,9 @@
+//
+//  UserAction.swift
+//  FullCircle
+//
+//  Created by Sunni Tang on 7/28/20.
+//  Copyright © 2020 FullCircle. All rights reserved.
+//
+
+import Foundation
