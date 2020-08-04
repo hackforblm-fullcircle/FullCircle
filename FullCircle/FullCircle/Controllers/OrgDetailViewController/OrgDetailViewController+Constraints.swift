6e313e655fd0c574edc444028a2472a8bd511212
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-08-04 13:28:54 -0400

    adds objects to subview

--- FullCircle/FullCircle/Controllers/OrgDetailViewController/OrgDetailViewController+Constraints.swift
+++ FullCircle/FullCircle/Controllers/OrgDetailViewController/OrgDetailViewController+Constraints.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2020 FullCircle. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension OrgDetailViewController {
+    func addSubviews() {
+        view.addSubview(orgImageView)
+        view.addSubview(orgNameLabel)
+        view.addSubview(orgDescriptionTextView)
+        view.addSubview(contactUsLabel)
+        view.addSubview(actionsLabel)
+        view.addSubview(callButton)
+        view.addSubview(emailButton)
+        view.addSubview(websiteButton)
+        view.addSubview(donateButton)
+        view.addSubview(messageButton)
+        view.addSubview(buttonStackView)
+        view.addSubview(orgActionsTableView)
+    }
+    
+    func addConstraints() {
+        setOrgImageViewConstraints()
+        
+    }
+    
+    
+    private func setOrgImageViewConstraints() {
+        
+    }
+}
