21f4f396b0dbe71e66b4ea2e2a004e208a3f94b4
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-08-04 13:28:18 -0400

    creates OrgDetailViewController+Constraints.swift

---
+++ FullCircle/FullCircle/Controllers/OrgDetailViewController/OrgDetailViewController+Constraints.swift
@@ -0,0 +1,9 @@
+//
+//  OrgDetailViewController+Constraints.swift
+//  FullCircle
+//
+//  Created by Sunni Tang on 8/4/20.
+//  Copyright © 2020 FullCircle. All rights reserved.
+//
+
+import Foundation
