a837e6dd565925cce3e24dfb01aa683593646441
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-08-11 16:32:12 -0400

    creates OrgDetailView.siwft and refactors OrgDetailViewController for programmatic UI

--- FullCircle/FullCircle/Controllers/OrgDetailViewController/OrgDetailViewController.swift
+++ FullCircle/FullCircle/Controllers/OrgDetailViewController/OrgDetailViewController.swift
@@ -2,111 +2,37 @@
 //  OrgDetailViewController.swift
 //  FullCircle
 //
-//  Created by Aishat Olowoshile on 6/13/20.
+//  Created by Sunni Tang on 8/4/20.
 //  Copyright © 2020 FullCircle. All rights reserved.
 //
 
 import UIKit
-import MapKit
-import CellDataCoupler
 
 class OrgDetailViewController: UIViewController {
     
-    @IBOutlet weak var tableView: UITableView!
-    var tableSource: TableSource?
+    //MARK: - UI Objects
+    let orgDetailView = OrgDetailView()
+    
+    var actionsLabel: FCSubHeaderLabel = {
+        let label = FCSubHeaderLabel()
+        label.text = "Actions"
+        return label
+    }()
+    
+    //TODO: Set up tableview
+    var orgActionsTableView = UITableView()
+    
+    //MARK: - Internal Properties
     var organization: Organization!
     
-    //Initializer
-    static func create(_ organization: Organization) -> OrgDetailViewController {
-        let vc = UIStoryboard(name: "OrgDetail", bundle: nil).instantiateViewController(withIdentifier: "OrgDetailViewController") as! OrgDetailViewController
-        vc.organization = organization
-        return vc
-    }
     
-    //Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
-        setupTableSource()
-        refreshUI()
+        view.backgroundColor = FCDesign.medGrey
+        self.navigationItem.title = "FullCircle"
+        
+        addSubviews()
+        addConstraints()
     }
     
-    //Setup
-    func setupUI() {
-        tableView.backgroundColor = .white
-    }
-    
-    func setupTableSource() {
-        tableSource = TableSource(with: tableView)
-        tableView.register(ActionListTableViewCell.self, forCellReuseIdentifier: "actionListCell")
-    }
-    
-    func refreshUI() {
-        displayItem(self.organization)
-    }
 }
-
-
-
-extension OrgDetailViewController {
-    func displayItem(_ organization: Organization) {
-        var couplers = [BaseCellCoupler]()
-        
-        //Logo
-        // TODO: Get image from Firebase Storage
-        //        let orgLogo = OrgDetailImageCellData(image: organization.imageURL)
-//        let orgLogoCell = CellCoupler(OrgDetailImageCell.self, orgLogo)
-//        couplers.append(orgLogoCell)
-        
-        //Name & Description
-        let orgNameAndDescrip = OrgDetailCellInfo(title: organization.name, description: organization.description)
-        let orgNameAndDescripCell = CellCoupler(OrgDetailCell.self, orgNameAndDescrip)
-        couplers.append(orgNameAndDescripCell)
-    
-        
-        //header
-        let orgAction = OrgDetailCellInfo(title: "Contact Us")
-        let orgActionCell = CellCoupler(OrgDetailCell.self, orgAction)
-        couplers.append(orgActionCell)
-        
-        //contact
-        let orgCoordinates = getCoordinates(forAddress: organization.address)
-        let orgContactInfo = OrgDetailContactCellData(phoneInfo: organization.phone, emailInfo: organization.email, linkInfo: organization.websiteURL, locationCoordinates: orgCoordinates)
-        let orgContactInfoCell = CellCoupler(OrgDetailContactCell.self, orgContactInfo)
-        couplers.append(orgContactInfoCell)
-        
-        
-        let donateToOrgInfo = OrgDetailButtonCellData(donationLink: organization.donateURL)
-        let buttonActioncell = CellCoupler(OrgDetailButtonCell.self, donateToOrgInfo)
-        couplers.append(buttonActioncell)
-        
-        //header
-        let orgTileAction = OrgDetailCellInfo(title: "Avaliable Actions")
-        let orgTitleActionCell = CellCoupler(OrgDetailCell.self, orgTileAction)
-        couplers.append(orgTitleActionCell)
-        
-        for action in organization.actions {
-            let orgActionName = OrgActionListCellData(actionName: action.name)
-            let orgActionNameCell = CellCoupler(OrgActionListCell.self, orgActionName)
-            couplers.append(orgActionNameCell)
-        }
-        
-        tableSource?.set(couplers: couplers)
-    }
-}
-
-extension OrgDetailViewController {
-    func getCoordinates(forAddress address: String) -> CLLocationCoordinate2D? {
-        let geocoder = CLGeocoder()
-        var coordinates: CLLocationCoordinate2D?
-        geocoder.geocodeAddressString(address) { (placemarks, error) in
-            guard error == nil else {
-                print("Geocoding error: \(error!)")
-                return
-            }
-            coordinates = placemarks?.first?.location?.coordinate
-        }
-        return coordinates
-    }
-}
-
---
+++ FullCircle/FullCircle/Views/OrgDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  OrgDetailView.swift
+//  FullCircle
+//
+//  Created by Sunni Tang on 8/11/20.
+//  Copyright © 2020 FullCircle. All rights reserved.
+//
+
+import UIKit
+
+class OrgDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
