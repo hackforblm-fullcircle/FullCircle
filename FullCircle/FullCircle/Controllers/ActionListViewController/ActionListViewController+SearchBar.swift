5b811e76ef7b45c31bdea15e2326b4a2f05a81f2
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-08-25 14:30:37 -0400

    creates ActionListViewController.swift+SearchBar

---
+++ FullCircle/FullCircle/Controllers/ActionListViewController/ActionListViewController+SearchBar.swift
@@ -0,0 +1,9 @@
+//
+//  ActionListViewController+SearchBar.swift
+//  FullCircle
+//
+//  Created by Sunni Tang on 8/25/20.
+//  Copyright © 2020 FullCircle. All rights reserved.
+//
+
+import Foundation
