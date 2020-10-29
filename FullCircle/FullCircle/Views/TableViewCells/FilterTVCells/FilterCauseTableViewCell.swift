3e6b6a0e20c6b61dd31ad79ebf73c78ae4b9fa21
Sunni Tang <sunni-tang@Sunnis-MacBook-Pro.local>
2020-10-29 16:27:28 -0400

    moves FilterVC and extensions to its own directory group

---
+++ FullCircle/FullCircle/Views/TableViewCells/FilterTVCells/FilterCauseTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FilterCauseTableViewCell.swift
+//  FullCircle
+//
+//  Created by Sunni Tang on 10/29/20.
+//  Copyright © 2020 FullCircle. All rights reserved.
+//
+
+import UIKit
+
+class FilterCauseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
